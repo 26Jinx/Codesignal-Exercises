b93a498ba4ca6d4fb7237ec482fcb4140b9782c0
PharahMain <26randomforests@gmail.com>
2020-12-18 11:20:04 -0800

    solving add border

---
+++ arcade/add_border.py
@@ -0,0 +1,22 @@
+# https://app.codesignal.com/arcade/intro/level-4/ZCD7NQnED724bJtjN
+
+def addBorder(picture):
+    # let's first determine the size of the picture
+    picture_height = len(picture)
+    picture_width = len(picture[0])
+    
+    # adding a picture frame
+    framed_picture = []
+    
+    frame_height = picture_height + 2
+    frame_width = picture_width + 2
+    
+    top = bottom = '*' * frame_width
+    for el in picture:
+        framed_picture.append('*' + el + '*')
+    
+    # now let's add the frame to the picture    
+    framed_picture.insert(0, top)
+    framed_picture.append(bottom)
+    
+    return framed_picture
